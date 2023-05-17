3abda62f35ccbe4f183d25dcc39673034509cf5a
Marcel <mrezende@bcfsolutions.net.br>
2023-05-17 11:29:31 -0300

    feat: adding stripe to the application

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import GoogleProvider from "next-auth/providers/google";
+import Stripe from "stripe";
 
 const prisma = new PrismaClient();
 
@@ -14,4 +14,23 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  events: {
+    createUser: async ({ user }) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+        apiVersion: "2022-11-15",
+      });
+      // create stripe customer
+      if (user.name && user.email) {
+        const costumer = await stripe.customers.create({
+          email: user.email,
+          name: user.name,
+        });
+        // update prisma user with stripe id
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { stripeCustomerId: costumer.id },
+        });
+      }
+    },
+  },
 });
